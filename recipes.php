0836d0a2d30c63e4c6d68e67ed028e31fa260f29
Peter DeRuvo <peterderuvo@gmail.com>
2021-12-20 15:51:23 -0500

    Fixed Logic for storage of Ingredients

--- recipes.php
+++ recipes.php
@@ -31,16 +31,20 @@
 
             public function displayRecipe(){
               if ($this->title!= "") {
-                # code...
               
-              echo "<h2>" . $this->title. "</h2>" ."Created by: " . $this->userId . "<br><br>" . $this->description . "<br><br>" .$this->instructions. "<br>" ;
-              
-                echo  $this->ingredient[$i]. " - Amount: " . $amount[$i]. " " . $unitOfMeasure[$i] . "<br>";
-              
-              
+              echo "<h2>" . $this->title. "</h2>" ."Created by: " . $this->userId . "<br><br>" . $this->description . "<br><br>" ;
+             for ($i=0; $i <sizeof($this->ingredient) ; $i++) { 
+               echo "-". $this->ingredient[$i] . ": " . $this->amount[$i]. " " . $this->unitOfMeasure[$i] . "<br>";
+             } 
+              echo  "<br><br>" .$this->instructions. "<br><br>";
+            }
             }
           }
-          }
+
+          
+      
+          
+          
         
           $Recipes[] = array();
                            
@@ -52,17 +56,15 @@
          FROM recipe r        
          JOIN users u on r.user_id = u.user_id;";
          $result = $conn->query($sql);
-         $x= 0; 
-         $z=0;
-         
+         $x= 0;
          if($result->num_rows > 0){
                             
              while($row = $result->fetch_assoc()){
-                   
+              
              $Recipes[$x]= new Recipe();
- 
+
              $titles[$x] = $row["Recipe"];
-             $rid[$x] = $row["id"];
+             $rid[$x] = $row["id"];  
              $instructions[$x] = $row["instructions"];
              $description[$x] = $row["description"];
              $useruid[$x] =$row["User"];
@@ -70,30 +72,65 @@
  
  
                }
-            }   
-            
-              
-        
-        else {
-            echo "<p>no Rows Found</p>";
-        }
+            } else {
+                      echo "<p>no Rows Found</p>";
+                    }
         for ($y=0; $y <sizeof($titles);$y++){
             if($rid[$y]!==$rid[$y-1]){
               $Recipes[$y]->title = $titles[$y];
               $Recipes[$y]->userId = $useruid[$y];
               $Recipes[$y]->instructions=$instructions[$y];
               $Recipes[$y]->description =$description[$y];
-        
+              $Recipes[$y]->recipeId = $rid[$y];
+              
+              
             }
-           if($rid[$y] == $rid[$y+1] || $rid[$y]==$rid[$y-1] || $rid[$y] == $rid[0]) {
-             $Recipes[$y]->ingredient[$z] = $ingredient[$y]; 
-             $Recipes[$y]->amount[$z] = $amount[$y]; 
-             $Recipes[$y]->unitOfMeasure[$z] = $unitOfMeasure[$y]; 
-              $z++;
-        
+         
         }
-    }
-    usort($Recipes, "my_sort");
+        foreach ($Recipes as $value) {
+         
+        
+        $rid = $value->recipeId;
+        $count=0;  
+        $sql = "SELECT 
+        i.name AS 'Ingredient',
+        ri.amount AS 'Amount',
+        mu.name AS 'Unit of Measure',
+        u.user_uid AS 'User'
+        FROM recipe r
+        JOIN recipeingredient ri ON r.id = ri.recipe_id
+        JOIN ingredient i on i.id = ri.ingredient_id
+        JOIN users u on r.user_id = u.user_id
+        LEFT OUTER JOIN measure mu on mu.id = measure_id
+        WHERE r.id = ?;";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $rid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+
+        if($result->num_rows > 0){
+
+        while($row = $result->fetch_assoc()){
+          $value->ingredient[$count] = $row["Ingredient"];
+          $value ->amount[$count] = $row["Amount"];
+          $value ->unitOfMeasure[$count] = $row["Unit of Measure"]; 
+          $count++;
+        }
+      }
+
+         }
+
+
+
+         
+        
+        
+      
+  
+
+   usort($Recipes, "my_sort");
     foreach ($Recipes as $value) {
       $value->displayRecipe();
     }
