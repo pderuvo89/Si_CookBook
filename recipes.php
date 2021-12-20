6ab83e1edbf4372be28226c7cb85c105395a8fc2
Peter DeRuvo <peterderuvo@gmail.com>
2021-12-20 10:10:43 -0500

    Logic changed for storing Ingredients

--- recipes.php
+++ recipes.php
@@ -11,70 +11,97 @@
     <?php
          require_once 'includes/connect.php';
 
+         function my_sort($a,$b)
+         {
+         if ($a==$b) return 0;
+         return ($a<$b)?-1:1;
+         }
+         
+         
+
           class Recipe{
             public $title;
             public $recipeId;
             public $description;
             public $instructions;
-            public $amount;
-            public $unitOfMeasure;
+            public $amount = array();
+            public $unitOfMeasure= array();
             public $ingredient = array();
             public $userId;
 
+            public function displayRecipe(){
+              if ($this->title!= "") {
+                # code...
+              
+              echo "<h2>" . $this->title. "</h2>" ."Created by: " . $this->userId . "<br><br>" . $this->description . "<br><br>" .$this->instructions. "<br>" ;
+              
+                echo  $this->ingredient[$i]. " - Amount: " . $amount[$i]. " " . $unitOfMeasure[$i] . "<br>";
+              
+              
+            }
+          }
           }
         
-          $Recipes = array();
+          $Recipes[] = array();
                            
-         $sql = "SELECT r.name AS 'Recipe', 
-         r.id,
-         r.description,
-         r.instructions, 
-         u.user_uid As 'User'
-        FROM recipe r 
-        JOIN users u on r.user_id = u.user_id";
-        $result = $conn->query($sql);
-        $x= 0; 
-
-        
-        if($result->num_rows > 0){
-                           
-            while($row = $result->fetch_assoc()){
-                  
-              $Recipes[$x] = new Recipe();
+          $sql = "SELECT r.name AS 'Recipe', 
+          r.id,
+          r.instructions,
+          r.description, 
+          u.user_uid As 'User'
+         FROM recipe r        
+         JOIN users u on r.user_id = u.user_id;";
+         $result = $conn->query($sql);
+         $x= 0; 
+         $z=0;
+         
+         if($result->num_rows > 0){
+                            
+             while($row = $result->fetch_assoc()){
+                   
+             $Recipes[$x]= new Recipe();
+ 
+             $titles[$x] = $row["Recipe"];
+             $rid[$x] = $row["id"];
+             $instructions[$x] = $row["instructions"];
+             $description[$x] = $row["description"];
+             $useruid[$x] =$row["User"];
+             $x++;   
+ 
+ 
+               }
+            }   
+            
               
-            $Recipes[$x]->$title = $row["Recipe"];
-            echo $Recipes[$x]->$title;
-            $Recipes[$x]->$recipeId = $row["id"];
-            $Recipes[$x]->$description = $row["description"];
-            $Recipes[$x]->$instructions = $row["instructions"];
-            $Recipes[$x]->$amount = $row["Amount"];
-            $Recipes[$x]->$unitOfMeasure = $row["Unit of Measure"];
-            $Recipes[$x]->$ingredient[$x] = $row["Ingredient"];  
-            $Recipes[$x]->$userId =$row["User"];
-            $x++;   
-
-            }
-           }   
         
         else {
             echo "<p>no Rows Found</p>";
         }
-       // for ($y=0; $y <sizeof($titles);$y++){
-       //     if($rid[$y]!==$rid[$y-1]){
-       // echo "<h2>" .$titles[$y]. "</h2>" ."Created by: " . $useruid[$y] . "<br><br>" . $description[$y] . "<br><br>" . $instructions[$y]. "<br>" ; 
-       //     }
-       //     if($rid[$y] == $rid[$y+1] || $rid[$y]==$rid[$y-1] || $rid[$y] == $rid[0]) {
-       //         echo $ingredient[$y]. " - Amount: " . $amount[$y] . " " . $unitofmeasure[$y] . "<br>";
+        for ($y=0; $y <sizeof($titles);$y++){
+            if($rid[$y]!==$rid[$y-1]){
+              $Recipes[$y]->title = $titles[$y];
+              $Recipes[$y]->userId = $useruid[$y];
+              $Recipes[$y]->instructions=$instructions[$y];
+              $Recipes[$y]->description =$description[$y];
         
-       // }
-    //}
-    //    foreach($Recipes as $recipe){
-    //      echo "<h2>" . $recipe->$instructions . "</h2>";
-    //    }
+            }
+           if($rid[$y] == $rid[$y+1] || $rid[$y]==$rid[$y-1] || $rid[$y] == $rid[0]) {
+             $Recipes[$y]->ingredient[$z] = $ingredient[$y]; 
+             $Recipes[$y]->amount[$z] = $amount[$y]; 
+             $Recipes[$y]->unitOfMeasure[$z] = $unitOfMeasure[$y]; 
+              $z++;
+        
+        }
+    }
+    usort($Recipes, "my_sort");
+    foreach ($Recipes as $value) {
+      $value->displayRecipe();
+    }
+
 
 
     ?>
-
+  <br>
 
   </div>
 </div>
